3a17ceeee3f34226faf6154baf4536b831711509
Rémi Cauchon <rcauchon@users.noreply.github.com>
2021-09-23 15:14:27 -0400

    Create db.php

---
+++ php-hello/db.php
@@ -0,0 +1,21 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+
+print " Host: ".$dbhost. "<br>";
+/*
+print " Host: " .$_ENV["INFODB_SERVICE_HOST"].  " DBname:" .$_ENV["POSTGRESQL_DATABASE"]. " User:" .$_ENV["POSTGRESQL_USER"]. " Password:" . $_ENV["POSTGRESQL_PASSWORD"]. "<br>";
+*/
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
+}
+$connection->close();
+?>
