5a7d36fb400427436e5dd98ad92ec3b399720295
Rémi Cauchon <rcauchon@users.noreply.github.com>
2021-09-23 10:26:12 -0400

    new print line again

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -15,7 +15,7 @@ $dbname      = $_ENV["POSTGRESQL_DATABASE"];
 $user        = $_ENV["POSTGRESQL_USER"];
 $password    = $_ENV["POSTGRESQL_PASSWORD"];
 
-print "Host: " .$_ENV["INFODB_SERVICE_HOST"].  " DBname:" .$_ENV["POSTGRESQL_DATABASE"]. " User:$_ENV["POSTGRESQL_USER"]". " Password:" . $_ENV["POSTGRESQL_PASSWORD"]. "<br>";
+print "Host: " .$_ENV["INFODB_SERVICE_HOST"].  " DBname:" .$_ENV["POSTGRESQL_DATABASE"]. " User:" .$_ENV["POSTGRESQL_USER"]. " Password:" . $_ENV["POSTGRESQL_PASSWORD"]. "<br>";
 
 if (!$connection = pg_connect ("host=$host dbname=$dbname user=$user password=$password")) {
     $error = error_get_last();
