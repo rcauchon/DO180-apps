a035cc0a861e1e97d93dc59d1f95efc38fd42355
Rémi Cauchon <rcauchon@users.noreply.github.com>
2021-09-23 09:09:06 -0400

    Update index.php
    new stuff

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,16 @@
 <?php
 print "Hello, World! Again php version is " . PHP_VERSION . "\n";
+
+$host        = INFODB_SERVICE_HOST;
+$port        = INFODB_SERVICE_PORT_POSTGRESQL;
+$dbname      = POSTGRESQL_DATABASE;
+$user        = POSTGRESQL_USER;
+$password    = POSTGRESQL_PASSWORD;
+
+if (!$connection = pg_connect ("host=$host dbname=$dbname user=$user password=$password")) {
+    $error = error_get_last();
+    echo "Connection failed. Error was: ". $error['message']. "\n";
+} else {
+    echo "Connection succesful.\n";
+}
 ?>
